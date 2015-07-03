30b628d2d3284d5667d1040b254c0948dd3c0030
MattiaCiolli <mattia93c@gmail.com>
2015-07-03 18:04:40 +0200

    slideout decente e templates sistemate

--- tt/www/js/views/pages/HomeView.js
+++ tt/www/js/views/pages/HomeView.js
@@ -3,6 +3,7 @@ define(function(require) {
   var Backbone = require("backbone");
   var Utils = require("utils");
   var Slideout = require("slideout");
+  var slick = require("slick");
 	
 var HomeView = Utils.Page.extend({
 
@@ -14,8 +15,9 @@ var HomeView = Utils.Page.extend({
     initialize: function() {
       // load the precompiled template
       this.template = Utils.templates.home;
-      // here we can register to inTheDOM or removing events
-      // this.listenTo(this, "inTheDOM", function() {
+	  // here we can register to inTheDOM or removing events
+		 	
+       // this.listenTo(this, "inTheDOM", function() {
       //   $('#content').on("swipe", function(data){
       //     console.log(data);
       //   });
@@ -38,18 +40,28 @@ var HomeView = Utils.Page.extend({
     render: function() {
 		
       $(this.el).html(this.template());
+		
+		//  SLICK            /////////////////////////////////////////////////////////
+		
+		this.$el.find('.slider').slick({
+      		dots: true,
+  			infinite: true,
+  			slidesToShow: 3,
+  			slidesToScroll: 3
+		});
+		
+		//  FINE SLICK       ////////////////////////////////////////////////////////
+		
       var slideout = new Slideout({
         'panel': this.$el.find("#main")[0],//document.getElementById('panel'),
         'menu': this.$el.find("#menu")[0],//document.getElementById('menu'),
         'padding': 256,
         'tolerance': 70
       });
-		this.$el.find("#click").on('click', slideout.close(), this);
 		
       return this;
     },
 	
-	click: function(e) {},
 	
 	 search: function(e) {
       Backbone.history.navigate("search", {
